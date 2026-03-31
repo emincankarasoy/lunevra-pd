eede967feb4367340d7c51e83353f9f8891dcd02
emincankarasoy <emincannkarasoy@gmail.com>
2026-03-31 19:06:21 +0300

    collection toggle, gender/season/scent filters, padding

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,7 @@
 export type Gender = "erkek" | "kadin" | "unisex";
 export type Collection = "essentielle" | "privee";
+export type Season = "ilkbahar-yaz" | "sonbahar-kis" | "tum-mevsimler";
+export type ScentFamily = "taze" | "odunsu" | "tatli" | "ciceksi" | "baharatli" | "meyveli";
 
 export interface Perfume {
   id: number;
@@ -8,59 +10,68 @@ export interface Perfume {
   similarTo: string;
   similarBrand: string;
   gender: Gender;
+  season: Season;
+  scentFamily: ScentFamily;
   productUrl: string;
 }
 
 export const perfumes: Perfume[] = [
   // ---- ERKEK ----
-  { id: 1, essentielleName: "Heritage", priveeName: "Indompté", similarTo: "Sauvage", similarBrand: "Dior", gender: "erkek", productUrl: "#" },
-  { id: 2, essentielleName: "Devotion", priveeName: "Éclat Absolu", similarTo: "Le Male Elixir", similarBrand: "Jean Paul Gaultier", gender: "erkek", productUrl: "#" },
-  { id: 3, essentielleName: "Alliance", priveeName: "Essence d'Amour", similarTo: "Stronger with You Intensely", similarBrand: "Emporio Armani", gender: "erkek", productUrl: "#" },
-  { id: 4, essentielleName: "Salient", priveeName: "Mystère", similarTo: "Y Eau de Parfum", similarBrand: "Yves Saint Laurent", gender: "erkek", productUrl: "#" },
-  { id: 5, essentielleName: "Passion", priveeName: "Nuit d'Éros", similarTo: "Eros", similarBrand: "Versace", gender: "erkek", productUrl: "#" },
-  { id: 6, essentielleName: "Bourbon", priveeName: "Poussière d'Étoile", similarTo: "Althair", similarBrand: "Parfums de Marly", gender: "erkek", productUrl: "#" },
-  { id: 7, essentielleName: "Marine", priveeName: "Vent de Liberté", similarTo: "Bleu De Chanel", similarBrand: "Chanel", gender: "erkek", productUrl: "#" },
-  { id: 8, essentielleName: "Refined", priveeName: "Écho d'Homme", similarTo: "Reflection Man", similarBrand: "Amouage", gender: "erkek", productUrl: "#" },
-  { id: 9, essentielleName: "Zenith", priveeName: "Victorieux", similarTo: "Invictus", similarBrand: "Paco Rabanne", gender: "erkek", productUrl: "#" },
-  { id: 10, essentielleName: "Celeste", priveeName: "Marque Éternelle", similarTo: "Code Profumo", similarBrand: "Armani", gender: "erkek", productUrl: "#" },
-  { id: 11, essentielleName: "Paragon", priveeName: "L'Impérial", similarTo: "Aventus", similarBrand: "Creed", gender: "erkek", productUrl: "#" },
-  { id: 12, essentielleName: "Grande", priveeName: "Monsieur Élégant", similarTo: "Gentleman", similarBrand: "Givenchy", gender: "erkek", productUrl: "#" },
-  { id: 13, essentielleName: "Piquant", priveeName: "Élixir Vanillé", similarTo: "Tobacco Vanille", similarBrand: "Tom Ford", gender: "erkek", productUrl: "#" },
-  { id: 14, essentielleName: "Magnate", priveeName: null, similarTo: "Imagination", similarBrand: "Louis Vuitton", gender: "erkek", productUrl: "#" },
-  { id: 15, essentielleName: "Verdant", priveeName: null, similarTo: "Torino 21", similarBrand: "Xerjoff", gender: "erkek", productUrl: "#" },
-  { id: 16, essentielleName: "Arcane", priveeName: null, similarTo: "Le Beau Le Parfum", similarBrand: "Jean Paul Gaultier", gender: "erkek", productUrl: "#" },
-  { id: 17, essentielleName: "Regnant", priveeName: null, similarTo: "Bois Imperial", similarBrand: "Essential Parfums", gender: "erkek", productUrl: "#" },
+  { id: 1, essentielleName: "Heritage", priveeName: "Indompté", similarTo: "Sauvage", similarBrand: "Dior", gender: "erkek", season: "tum-mevsimler", scentFamily: "taze", productUrl: "#" },
+  { id: 2, essentielleName: "Devotion", priveeName: "Éclat Absolu", similarTo: "Le Male Elixir", similarBrand: "Jean Paul Gaultier", gender: "erkek", season: "sonbahar-kis", scentFamily: "tatli", productUrl: "#" },
+  { id: 3, essentielleName: "Alliance", priveeName: "Essence d'Amour", similarTo: "Stronger with You Intensely", similarBrand: "Emporio Armani", gender: "erkek", season: "sonbahar-kis", scentFamily: "tatli", productUrl: "#" },
+  { id: 4, essentielleName: "Salient", priveeName: "Mystère", similarTo: "Y Eau de Parfum", similarBrand: "Yves Saint Laurent", gender: "erkek", season: "ilkbahar-yaz", scentFamily: "taze", productUrl: "#" },
+  { id: 5, essentielleName: "Passion", priveeName: "Nuit d'Éros", similarTo: "Eros", similarBrand: "Versace", gender: "erkek", season: "ilkbahar-yaz", scentFamily: "taze", productUrl: "#" },
+  { id: 6, essentielleName: "Bourbon", priveeName: "Poussière d'Étoile", similarTo: "Althair", similarBrand: "Parfums de Marly", gender: "erkek", season: "sonbahar-kis", scentFamily: "tatli", productUrl: "#" },
+  { id: 7, essentielleName: "Marine", priveeName: "Vent de Liberté", similarTo: "Bleu De Chanel", similarBrand: "Chanel", gender: "erkek", season: "tum-mevsimler", scentFamily: "odunsu", productUrl: "#" },
+  { id: 8, essentielleName: "Refined", priveeName: "Écho d'Homme", similarTo: "Reflection Man", similarBrand: "Amouage", gender: "erkek", season: "ilkbahar-yaz", scentFamily: "taze", productUrl: "#" },
+  { id: 9, essentielleName: "Zenith", priveeName: "Victorieux", similarTo: "Invictus", similarBrand: "Paco Rabanne", gender: "erkek", season: "ilkbahar-yaz", scentFamily: "taze", productUrl: "#" },
+  { id: 10, essentielleName: "Celeste", priveeName: "Marque Éternelle", similarTo: "Code Profumo", similarBrand: "Armani", gender: "erkek", season: "sonbahar-kis", scentFamily: "baharatli", productUrl: "#" },
+  { id: 11, essentielleName: "Paragon", priveeName: "L'Impérial", similarTo: "Aventus", similarBrand: "Creed", gender: "erkek", season: "ilkbahar-yaz", scentFamily: "meyveli", productUrl: "#" },
+  { id: 12, essentielleName: "Grande", priveeName: "Monsieur Élégant", similarTo: "Gentleman", similarBrand: "Givenchy", gender: "erkek", season: "tum-mevsimler", scentFamily: "odunsu", productUrl: "#" },
+  { id: 13, essentielleName: "Piquant", priveeName: "Élixir Vanillé", similarTo: "Tobacco Vanille", similarBrand: "Tom Ford", gender: "erkek", season: "sonbahar-kis", scentFamily: "tatli", productUrl: "#" },
+  { id: 14, essentielleName: "Magnate", priveeName: null, similarTo: "Imagination", similarBrand: "Louis Vuitton", gender: "erkek", season: "ilkbahar-yaz", scentFamily: "taze", productUrl: "#" },
+  { id: 15, essentielleName: "Verdant", priveeName: null, similarTo: "Torino 21", similarBrand: "Xerjoff", gender: "erkek", season: "ilkbahar-yaz", scentFamily: "taze", productUrl: "#" },
+  { id: 16, essentielleName: "Arcane", priveeName: null, similarTo: "Le Beau Le Parfum", similarBrand: "Jean Paul Gaultier", gender: "erkek", season: "ilkbahar-yaz", scentFamily: "tatli", productUrl: "#" },
+  { id: 17, essentielleName: "Regnant", priveeName: null, similarTo: "Bois Imperial", similarBrand: "Essential Parfums", gender: "erkek", season: "tum-mevsimler", scentFamily: "odunsu", productUrl: "#" },
 
   // ---- KADIN ----
-  { id: 24, essentielleName: "Sublime", priveeName: null, similarTo: "Erba Pura", similarBrand: "Xerjoff", gender: "kadin", productUrl: "#" },
-  { id: 30, essentielleName: "Radiant", priveeName: "Charme Absolu", similarTo: "Goddess", similarBrand: "Burberry", gender: "kadin", productUrl: "#" },
-  { id: 25, essentielleName: "Candour", priveeName: null, similarTo: "My Way", similarBrand: "Giorgio Armani", gender: "kadin", productUrl: "#" },
-  { id: 31, essentielleName: "Nomadic", priveeName: "Libre Lumière", similarTo: "Libre", similarBrand: "Yves Saint Laurent", gender: "kadin", productUrl: "#" },
-  { id: 26, essentielleName: "Elysian", priveeName: null, similarTo: "Fleur Narcotique", similarBrand: "Ex Nihilo", gender: "kadin", productUrl: "#" },
-  { id: 32, essentielleName: "Pristine", priveeName: "Muse Intemporelle", similarTo: "Coco Mademoiselle", similarBrand: "Chanel", gender: "kadin", productUrl: "#" },
-  { id: 27, essentielleName: "Amoret", priveeName: null, similarTo: "Bombshell", similarBrand: "Victoria's Secret", gender: "kadin", productUrl: "#" },
-  { id: 33, essentielleName: "Melliflu", priveeName: "Caramel Blanc", similarTo: "Eclaire", similarBrand: "Lattafa", gender: "kadin", productUrl: "#" },
-  { id: 28, essentielleName: "Crystal", priveeName: null, similarTo: "Bright Crystal", similarBrand: "Versace", gender: "kadin", productUrl: "#" },
-  { id: 34, essentielleName: "Dualism", priveeName: "Douce Demoiselle", similarTo: "Good Girl", similarBrand: "Carolina Herrera", gender: "kadin", productUrl: "#" },
-  { id: 29, essentielleName: "Veloute", priveeName: null, similarTo: "Eau Tendre", similarBrand: "Chanel", gender: "kadin", productUrl: "#" },
-  { id: 35, essentielleName: "Captive", priveeName: "Fleur de Bonheur", similarTo: "La Vie Est Belle", similarBrand: "Lancôme", gender: "kadin", productUrl: "#" },
+  { id: 24, essentielleName: "Sublime", priveeName: null, similarTo: "Erba Pura", similarBrand: "Xerjoff", gender: "kadin", season: "ilkbahar-yaz", scentFamily: "meyveli", productUrl: "#" },
+  { id: 30, essentielleName: "Radiant", priveeName: "Charme Absolu", similarTo: "Goddess", similarBrand: "Burberry", gender: "kadin", season: "sonbahar-kis", scentFamily: "tatli", productUrl: "#" },
+  { id: 25, essentielleName: "Candour", priveeName: null, similarTo: "My Way", similarBrand: "Giorgio Armani", gender: "kadin", season: "ilkbahar-yaz", scentFamily: "ciceksi", productUrl: "#" },
+  { id: 31, essentielleName: "Nomadic", priveeName: "Libre Lumière", similarTo: "Libre", similarBrand: "Yves Saint Laurent", gender: "kadin", season: "tum-mevsimler", scentFamily: "ciceksi", productUrl: "#" },
+  { id: 26, essentielleName: "Elysian", priveeName: null, similarTo: "Fleur Narcotique", similarBrand: "Ex Nihilo", gender: "kadin", season: "ilkbahar-yaz", scentFamily: "ciceksi", productUrl: "#" },
+  { id: 32, essentielleName: "Pristine", priveeName: "Muse Intemporelle", similarTo: "Coco Mademoiselle", similarBrand: "Chanel", gender: "kadin", season: "tum-mevsimler", scentFamily: "ciceksi", productUrl: "#" },
+  { id: 27, essentielleName: "Amoret", priveeName: null, similarTo: "Bombshell", similarBrand: "Victoria's Secret", gender: "kadin", season: "ilkbahar-yaz", scentFamily: "meyveli", productUrl: "#" },
+  { id: 33, essentielleName: "Melliflu", priveeName: "Caramel Blanc", similarTo: "Eclaire", similarBrand: "Lattafa", gender: "kadin", season: "sonbahar-kis", scentFamily: "tatli", productUrl: "#" },
+  { id: 28, essentielleName: "Crystal", priveeName: null, similarTo: "Bright Crystal", similarBrand: "Versace", gender: "kadin", season: "ilkbahar-yaz", scentFamily: "ciceksi", productUrl: "#" },
+  { id: 34, essentielleName: "Dualism", priveeName: "Douce Demoiselle", similarTo: "Good Girl", similarBrand: "Carolina Herrera", gender: "kadin", season: "sonbahar-kis", scentFamily: "tatli", productUrl: "#" },
+  { id: 29, essentielleName: "Veloute", priveeName: null, similarTo: "Eau Tendre", similarBrand: "Chanel", gender: "kadin", season: "ilkbahar-yaz", scentFamily: "ciceksi", productUrl: "#" },
+  { id: 35, essentielleName: "Captive", priveeName: "Fleur de Bonheur", similarTo: "La Vie Est Belle", similarBrand: "Lancôme", gender: "kadin", season: "tum-mevsimler", scentFamily: "tatli", productUrl: "#" },
 
   // ---- UNISEX ----
-  { id: 18, essentielleName: "Halcyon", priveeName: "Nuit Élysée", similarTo: "Naxos", similarBrand: "Xerjoff", gender: "unisex", productUrl: "#" },
-  { id: 19, essentielleName: "Gourme", priveeName: "Ivresse d'Or", similarTo: "Khamrah", similarBrand: "Lattafa", gender: "unisex", productUrl: "#" },
-  { id: 20, essentielleName: "Dynasty", priveeName: "Séduction Nocturne", similarTo: "Layton", similarBrand: "Parfums de Marly", gender: "unisex", productUrl: "#" },
-  { id: 21, essentielleName: "Mirage", priveeName: "Magie Noire", similarTo: "Kirke", similarBrand: "Tiziana Terenzi", gender: "unisex", productUrl: "#" },
-  { id: 22, essentielleName: "Ethereal", priveeName: "Améthyste Pourpre", similarTo: "Baccarat Rouge 540", similarBrand: "Maison Francis Kurkdjian", gender: "unisex", productUrl: "#" },
-  { id: 23, essentielleName: "Antique", priveeName: "Atlantide Perdue", similarTo: "Cappadocia", similarBrand: "Simone Andreoli", gender: "unisex", productUrl: "#" },
+  { id: 18, essentielleName: "Halcyon", priveeName: "Nuit Élysée", similarTo: "Naxos", similarBrand: "Xerjoff", gender: "unisex", season: "sonbahar-kis", scentFamily: "tatli", productUrl: "#" },
+  { id: 19, essentielleName: "Gourme", priveeName: "Ivresse d'Or", similarTo: "Khamrah", similarBrand: "Lattafa", gender: "unisex", season: "sonbahar-kis", scentFamily: "tatli", productUrl: "#" },
+  { id: 20, essentielleName: "Dynasty", priveeName: "Séduction Nocturne", similarTo: "Layton", similarBrand: "Parfums de Marly", gender: "unisex", season: "sonbahar-kis", scentFamily: "baharatli", productUrl: "#" },
+  { id: 21, essentielleName: "Mirage", priveeName: "Magie Noire", similarTo: "Kirke", similarBrand: "Tiziana Terenzi", gender: "unisex", season: "ilkbahar-yaz", scentFamily: "meyveli", productUrl: "#" },
+  { id: 22, essentielleName: "Ethereal", priveeName: "Améthyste Pourpre", similarTo: "Baccarat Rouge 540", similarBrand: "Maison Francis Kurkdjian", gender: "unisex", season: "tum-mevsimler", scentFamily: "tatli", productUrl: "#" },
+  { id: 23, essentielleName: "Antique", priveeName: "Atlantide Perdue", similarTo: "Cappadocia", similarBrand: "Simone Andreoli", gender: "unisex", season: "tum-mevsimler", scentFamily: "odunsu", productUrl: "#" },
 ];
 
 export function filterPerfumes(
   collection: Collection,
-  gender: "erkek" | "kadin",
+  gender: "erkek" | "kadin" | null,
+  season: Season | null,
+  scentFamily: ScentFamily | null,
 ): Perfume[] {
   return perfumes.filter((p) => {
     if (collection === "privee" && !p.priveeName) return false;
-    const genderMatch = p.gender === gender || p.gender === "unisex";
-    return genderMatch;
+    if (gender) {
+      const genderMatch = p.gender === gender || p.gender === "unisex";
+      if (!genderMatch) return false;
+    }
+    if (season && p.season !== season && p.season !== "tum-mevsimler") return false;
+    if (scentFamily && p.scentFamily !== scentFamily) return false;
+    return true;
   });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,59 @@
 import "./style.css";
 import { filterPerfumes } from "./data";
-import type { Collection } from "./data";
+import type { Collection, Season, ScentFamily } from "./data";
 import { renderTable } from "./table";
 
 const SEARCH_ICON = `<svg class="search-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>`;
 
 const app = document.getElementById("app")!;
-
 const PER_PAGE = 8;
 
-type Tab = { collection: Collection; gender: "erkek" | "kadin"; label: string };
-const tabs: Tab[] = [
-  { collection: "essentielle", gender: "erkek", label: "Essentielle Erkek" },
-  { collection: "essentielle", gender: "kadin", label: "Essentielle Kadın" },
-  { collection: "privee", gender: "erkek", label: "Privée Erkek" },
-  { collection: "privee", gender: "kadin", label: "Privée Kadın" },
-];
-
-let activeTab = 0;
+let activeCollection: Collection = "essentielle";
+let activeGender: "erkek" | "kadin" | null = null;
+let activeSeason: Season | null = null;
+let activeScent: ScentFamily | null = null;
 let currentPage = 1;
 let currentQuery = "";
 
+const genderOptions: { value: "erkek" | "kadin"; label: string }[] = [
+  { value: "erkek", label: "Erkek" },
+  { value: "kadin", label: "Kadın" },
+];
+
+const seasonOptions: { value: Season; label: string }[] = [
+  { value: "ilkbahar-yaz", label: "İlkbahar / Yaz" },
+  { value: "sonbahar-kis", label: "Sonbahar / Kış" },
+  { value: "tum-mevsimler", label: "Tüm Mevsimler" },
+];
+
+const scentOptions: { value: ScentFamily; label: string }[] = [
+  { value: "taze", label: "Taze" },
+  { value: "odunsu", label: "Odunsu" },
+  { value: "tatli", label: "Tatlı" },
+  { value: "ciceksi", label: "Çiçeksi" },
+  { value: "baharatli", label: "Baharatlı" },
+  { value: "meyveli", label: "Meyveli" },
+];
+
+function renderFilterGroup(
+  label: string,
+  options: { value: string; label: string }[],
+  active: string | null,
+  dataAttr: string,
+): string {
+  const btns = options
+    .map(
+      (o) =>
+        `<button class="filter-chip${o.value === active ? " active" : ""}" data-${dataAttr}="${o.value}">${o.label}</button>`
+    )
+    .join("");
+  return `<div class="filter-group"><span class="filter-label">${label}</span>${btns}</div>`;
+}
+
 function render() {
-  const tab = tabs[activeTab];
   const q = currentQuery.trim().toLowerCase();
 
-  let filtered = filterPerfumes(tab.collection, tab.gender);
+  let filtered = filterPerfumes(activeCollection, activeGender, activeSeason, activeScent);
 
   if (q) {
     filtered = filtered.filter(
@@ -41,29 +69,22 @@ function render() {
     ? `<p class="result-count">${filtered.length} sonuç bulundu</p>`
     : "";
 
-  const tabButtons = tabs
-    .map(
-      (t, i) =>
-        `<button class="tab-btn${i === activeTab ? " active" : ""}" data-tab="${i}">${t.label}</button>`
-    )
-    .join("");
-
-  const collectionInfo = tab.collection === "essentielle"
-    ? `<div class="collection-info">
-        <span class="collection-badge essentielle">Essentielle</span>
-        <p>Sevdiğiniz parfümlerin uygun fiyatlı muadilleri. Günlük kullanım için ideal.</p>
-      </div>`
-    : `<div class="collection-info">
-        <span class="collection-badge privee">Privée</span>
-        <p>Premium içeriklerle hazırlanan özel koleksiyon. Orijinaline en yakın deneyim.</p>
-      </div>`;
+  const collectionDesc = activeCollection === "essentielle"
+    ? "Sevdiğiniz parfümlerin uygun fiyatlı muadilleri. Günlük kullanım için ideal."
+    : "Premium içeriklerle hazırlanan özel koleksiyon. Orijinaline en yakın deneyim.";
 
   app.innerHTML = `
-    <div class="tabs">
-      ${tabButtons}
+    <div class="collection-toggle">
+      <button class="collection-btn${activeCollection === "essentielle" ? " active" : ""}" data-col="essentielle">Essentielle</button>
+      <button class="collection-btn${activeCollection === "privee" ? " active" : ""}" data-col="privee">Privée</button>
     </div>
+    <p class="collection-desc">${collectionDesc}</p>
 
-    ${collectionInfo}
+    <div class="filters">
+      ${renderFilterGroup("Cinsiyet", genderOptions, activeGender, "gender")}
+      ${renderFilterGroup("Mevsim", seasonOptions, activeSeason, "season")}
+      ${renderFilterGroup("Koku Ailesi", scentOptions, activeScent, "scent")}
+    </div>
 
     <div class="search-container">
       ${SEARCH_ICON}
@@ -77,19 +98,49 @@ function render() {
     </div>
 
     ${resultCount}
-    ${renderTable(filtered, tab.collection, currentPage, PER_PAGE)}
+    ${renderTable(filtered, activeCollection, currentPage, PER_PAGE)}
   `;
 
-  // Tab events
-  app.querySelectorAll<HTMLButtonElement>(".tab-btn").forEach((btn) => {
+  // Collection toggle
+  app.querySelectorAll<HTMLButtonElement>(".collection-btn").forEach((btn) => {
     btn.addEventListener("click", () => {
-      activeTab = Number(btn.dataset.tab);
+      activeCollection = btn.dataset.col as Collection;
       currentPage = 1;
       render();
     });
   });
 
-  // Search event
+  // Gender filter
+  app.querySelectorAll<HTMLButtonElement>("[data-gender]").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const val = btn.dataset.gender as "erkek" | "kadin";
+      activeGender = activeGender === val ? null : val;
+      currentPage = 1;
+      render();
+    });
+  });
+
+  // Season filter
+  app.querySelectorAll<HTMLButtonElement>("[data-season]").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const val = btn.dataset.season as Season;
+      activeSeason = activeSeason === val ? null : val;
+      currentPage = 1;
+      render();
+    });
+  });
+
+  // Scent filter
+  app.querySelectorAll<HTMLButtonElement>("[data-scent]").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const val = btn.dataset.scent as ScentFamily;
+      activeScent = activeScent === val ? null : val;
+      currentPage = 1;
+      render();
+    });
+  });
+
+  // Search
   const input = app.querySelector<HTMLInputElement>(".search-input")!;
   input.addEventListener("input", () => {
     currentQuery = input.value;
@@ -100,7 +151,7 @@ function render() {
     el.setSelectionRange(el.value.length, el.value.length);
   });
 
-  // Pagination events
+  // Pagination
   app.querySelectorAll<HTMLButtonElement>(".page-btn").forEach((btn) => {
     btn.addEventListener("click", () => {
       const page = Number(btn.dataset.page);
